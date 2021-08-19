206ee061d45d6b12fa1eec335d9320dd2380cf96
TheFifthUnknown <deathre921@gmail.com>
2021-08-19 19:34:10 +0700

    Update Program.cs

--- TestTaskProgrammerForward/Program.cs
+++ TestTaskProgrammerForward/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using TestTaskProgrammerForward.Creators;
 using TestTaskProgrammerForward.Enum;
+using TestTaskProgrammerForward.Interfaces;
+using TestTaskProgrammerForward.Model;
 
 namespace TestTaskProgrammerForward
 {
@@ -9,12 +11,36 @@ namespace TestTaskProgrammerForward
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("input ambient temperature");
-            double _temp = Convert.ToDouble(Console.ReadLine());
-            
             CreatorEngine creator = new CreatorEngine();
-            var engine = creator.GetEngine("Data.json", TypesOfEngins.InternalCombucstion); // Data.json лежит в папке debug
+            IEngine engine = null;
+            double _temp = 0;
+            bool flag = true; 
             
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Input ambient temperature");
+                    _temp = Convert.ToDouble(Console.ReadLine());
+                    flag = false;
+                }
+                catch(Exception e)
+                {
+                    Console.Clear();
+                    Console.WriteLine(e.Message);
+                }
+
+            } while (flag);
+   
+            try
+            {
+                engine = creator.GetEngine("Data.json", TypesOfEngins.InternalCombucstion); // Data.json лежит в папке debug
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
             Stand stand = new Stand(new List<TypesOfTests>() { TypesOfTests.CriticalTemperature });
             
             engine.Attach(stand);
